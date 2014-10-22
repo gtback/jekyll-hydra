152cc50610c24de9da0aa3d37e72636b990066eb
Greg Back <gback@mitre.org>
2014-10-21 21:57:18 -0500

    Find an available port to listen on

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import tempfile
 import threading
+import socket
 import subprocess
 
 from flask import Flask, render_template, redirect, url_for
@@ -13,6 +14,8 @@ from wtforms.validators import Required
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
+
+START_PORT = 4000
 
 db = SQLAlchemy(app)
 
@@ -55,14 +58,39 @@ def print_args(args):
     print(' '.join(args))
     print("-" * 40)
 
+
+def find_port():
+    """Find an open TCP port, starting at 4000 and working upward"""
+    # There's probably a race condition here.
+    found = False
+    port = START_PORT
+    while not found:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = s.connect_ex(('127.0.0.1', port))
+        print(result)
+        if result == 61:
+            print("Port %d is available" % port)
+            found = True
+        else:
+            print("Port %d in use" % port)
+            port = port + 1
+
+        s.close()
+
+        if port > START_PORT + 10:
+            raise ValueError("No usable port found")
+    return port
+
+
 def run_it(repository, branch):
     repository = repository.strip()
     branch = branch.strip()
     with tempfile.TemporaryDirectory() as tmpdirname:
-        print("created temporary directory", tmpdirname)
+        print("Created temporary directory", tmpdirname)
         repo_dir = os.path.join(tmpdirname, 'repo')
 
         # Clone the repo
+        print("Cloning")
         args = ['git', 'clone', repository, repo_dir]
         print_args(args)
         subprocess.call(args)
@@ -75,11 +103,9 @@ def run_it(repository, branch):
         print_args(args)
         subprocess.call(args)
 
-        args = ['bundle', 'install']
-        print_args(args)
-        subprocess.call(args)
-
-        args = ['jekyll', 'serve']
+        port = find_port()
+        print("Serving using port %d" % port)
+        args = ['jekyll', 'serve', '-P', str(port)]
         print_args(args)
         subprocess.call(args)
 
